caf04f6314c75048614a700d89ad05944320be93
adarsh-addy <adarsh80singh@gmail.com>
2022-07-12 12:46:15 +0530

    commit

--- script.js
+++ script.js
@@ -1,40 +1,51 @@
 var imgurl;
-// var cropped = document.getElementsByClassName("cropped"),
-//   img_result = document.getElementsByClassName("img-result");
+var cropped = document.getElementsByClassName('cropped'),
+    img_result = document.getElementsByClassName('img-result')
 function readURL(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      $("#blah").attr("src", e.target.result);
-    };
-    reader.readAsDataURL(input.files[0]);
-    setTimeout(initCropper, 1000);
-  }
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#blah').attr('src', e.target.result)
+        };
+        reader.readAsDataURL(input.files[0]);
+        setTimeout(initCropper, 1000);
+    }
 }
 function initCropper() {
-  console.log("Came here");
-  var image = document.getElementById("blah");
-  var cropper = new Cropper(image, {
-    aspectRatio: 16 / 9,
-    crop: function (e) {
-      console.log(e.detail.x);
-      console.log(e.detail.y);
-    },
-  });
+    console.log("Came here")
+    var image = document.getElementById('blah');
+    var cropper = new Cropper(image, {
+        aspectRatio: 16 / 9,
+        crop: function (e) {
+            console.log(e.detail.x);
+            console.log(e.detail.y);
+        }
+    });
 
-  // save on click
-  document.getElementById("save_button").addEventListener("click", (e) => {
-    e.preventDefault();
-    // get result to data uri
-    imgurl = cropper.getCroppedCanvas().toDataURL();
-    console.log(imgurl);
-    //showing the image
-    var img1 = document.createElement("img");
-    img1.src = imgurl;
-    document.getElementById("cropped_result").appendChild(img1);
-  });
+    // On crop button clicked
+    // document.getElementById('crop_button').addEventListener('click', function () {
+    //     imgurl = cropper.getCroppedCanvas().toDataURL();
+    //     console.log(imgurl);
+    //     var img = document.createElement("img");
+    //     img.src = imgurl;
+    //     document.getElementById("cropped_result").appendChild(img);
 
-  /* ---------------- SEND IMAGE TO THE SERVER-------------------------
+    // save on click
+    let flag=true;
+    document.getElementById('save_button').addEventListener('click', (e) => {
+        e.preventDefault();
+        // get result to data uri
+        if(flag){
+        imgurl = cropper.getCroppedCanvas().toDataURL();
+        console.log(imgurl);
+        var img1 = document.createElement("img");
+        img1.src = imgurl;
+        document.getElementById("cropped_result").appendChild(img1);
+        flag=false;
+        }
+    });
+
+    /* ---------------- SEND IMAGE TO THE SERVER-------------------------
  
                 cropper.getCroppedCanvas().toBlob(function (blob) {
                       var formData = new FormData();
@@ -54,5 +65,7 @@ function initCropper() {
                       });
                 });
             ----------------------------------------------------*/
-  //     })
+    //     })
 }
+
+
